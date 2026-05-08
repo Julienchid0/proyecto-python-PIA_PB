2c56136dc08d626e7f620e08f24590a3b22f3886
abi293 <nancy.barrerat@uanl.edu.mx>
2026-05-07 23:35:23 -0600

    Update procesamiento.py

--- procesamiento.py
+++ procesamiento.py
@@ -1,4 +1,4 @@
-def procesar_canciones(datos, limite=5):
+def procesar_canciones(datos, limite=10):
     if "data" not in datos:
         return []
 
@@ -7,6 +7,8 @@ def procesar_canciones(datos, limite=5):
             "titulo": c["title"],
             "artista": c["artist"]["name"],
             "album": c["album"]["title"]
+            "duracion_seg": c.get("duration", 0),
+            "rank": c.get("rank", 0)
         }
         for c in datos["data"][:limite]
     ]
