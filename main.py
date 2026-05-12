560dacc3886ada777f3a96d0665a537805e680f8
abi293 <nancy.barrerat@uanl.edu.mx>
2026-05-12 13:30:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from api_conexion import obtener_datos
+from api_deezer import obtener_datos
 from procesamiento import procesar_canciones
-from graficas import grafica_albumes, grafica_artistas
+from graficas import grafica_albumes, grafica_artistas, grafica_duracion
 
 def main():
-    datos = obtener_datos("soda stereo")
+    datos = obtener_datos()
     canciones = procesar_canciones(datos, limite=10)
 
     if not canciones:
